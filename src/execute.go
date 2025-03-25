975d1b74e3a5560343b8ef02ddfcab7163edf851
codeforge11 <codeforge11@gmail.com>
2025-03-25 20:44:19 +0100

    refactor: use guard clausue

--- src/execute.go
+++ src/execute.go
@@ -11,35 +11,34 @@ func (w *MainWindow) execute(_ bool) {
 	w.messageText.Clear()
 	w.statusLabel.SetText("")
 
-	if w.db != nil {
-		sqlCode := strings.TrimSpace(w.sqlEntry.ToPlainText())
-		queries := strings.Split(sqlCode, ";")
-		for _, query := range queries {
-			query = strings.TrimSpace(query)
-			if query == "" {
-				continue
-			}
-			rows, err := w.db.Query(query)
-			if err != nil {
-				w.displayMessage(fmt.Sprintf("SQL execution error: %s", err))
-				logError(err)
-				continue
-			}
-			defer rows.Close()
+	if w.db == nil {
+		w.statusLabel.SetText("Not connected to the database")
+		return
+	}
 
-			w.displayResults(rows)
+	sqlCode := strings.TrimSpace(w.sqlEntry.ToPlainText())
+	queries := strings.Split(sqlCode, ";")
+	for _, query := range queries {
+		query = strings.TrimSpace(query)
+
+		rows, err := w.db.Query(query)
+		if err != nil {
+			w.displayMessage(fmt.Sprintf("SQL execution error: %s", err))
+			logError(err)
+			continue
 		}
 
-		w.statusLabel.SetText("SQL executed successfully")
-
-		timer := core.NewQTimer(nil)
-		timer.SetSingleShot(true)
-		timer.ConnectTimeout(w.clearStatusLabel)
-		timer.Start(5000)
-
-	} else {
-		w.statusLabel.SetText("Not connected to the database")
+		if query != "" {
+			w.statusLabel.SetText("SQL executed successfully")
+			defer rows.Close()
+			w.displayResults(rows)
+		}
 	}
+
+	timer := core.NewQTimer(nil)
+	timer.SetSingleShot(true)
+	timer.ConnectTimeout(w.clearStatusLabel)
+	timer.Start(5000)
 }
 
 func (w *MainWindow) clearStatusLabel() {
